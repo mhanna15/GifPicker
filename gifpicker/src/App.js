05a812b7ac6e3b21dd12ae57a215ad70a7dafff4
ameya mellacheruvu <ameyacapps@gmail.com>
2020-03-04 16:28:36 -0800

    react-dom, moodwall pg added

--- gifpicker/src/App.js
+++ gifpicker/src/App.js
@@ -1,20 +1,62 @@
 import React from "react";
 import "./App.css";
 import SearchForm from "./searchForm";
-import Search from "./api";
+import MoodWall from "./MoodWall"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 
-function App() {
-  return (   
-    <div className="App"> 
-      <header className="App-header">
-        <h1>Welcome to MoodWall</h1>
-        <SearchForm />
-      </header>
-      
-    </div>
-    
-  );
+const App = () => {
+    return ( 
+    <Router>
+        <div className = "App" >
+            <Button component = { Link } to = "/MoodWall" >
+            <nav >
+                <NavLink activeClassName = "ActiveHomePage" exact id = "Home Page" to = "/" > Home 
+                </NavLink>
+            </nav>
+            </Button>  
+        
+        <nav >
+        <div name = "homeButton">
+        <Button component = { Link }to = "/Home" >
+            <NavLink activeClassName = "ActiveMoodWallPage" exact id = "MoodWallPage" to = "/MoodWall" > MoodWall 
+            </NavLink>
+         </Button> 
+         </div>
+         </nav>
+
+         
+
+        <Switch>
+            <Route exact path = "/" > <Home/>          
+            </Route> 
+            <Route exact path = "/MoodWall" component = { MoodWall } > <MoodWall/>
+            </Route>
+        </Switch>        
+        </div>
+    </Router>
+
+    );
+
+    function Home() {
+        return ( 
+            <div className = "Home" >
+                <header className = "App-header" >
+                <h1> Welcome to MoodWall </h1> 
+                <SearchForm/>
+                </header> 
+            </div>
+
+        )
+    }
 }
 
 export default App;
---
+++ gifpicker/src/moodWall.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './MoodWall.css';
+
+
+const MoodWall = () => { 
+    return (
+        <div>
+           
+        </div>
+
+    )
+
+}
+
+
+
+
+export default MoodWall
